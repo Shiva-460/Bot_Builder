88bf9b9d1f022f6f4300fe8219bc64dae54dfdd1
cmarisco <45523456+cmarisco@users.noreply.github.com>
2019-02-28 10:21:56 -0800

    cosmetic change

--- Motors/examples/motors-basic/motors-basic.ino
+++ Motors/examples/motors-basic/motors-basic.ino
@@ -12,15 +12,15 @@
 //Motors and Arduino interface with the L239D Dual H-Bridge Motor Driver.
 //Pin numberings based on the wiring diagram in referenced documentation.
 
-const int m1_en = 25; //Motor 1 PWM Speed Control Pin, Pin 1 on L239
-const int m2_en = 24; //Motor 2 PWM Speed Control Pin, Pin 9 on L239
+const int mr_en = 25; //Motor 1 PWM Speed Control Pin, Pin 1 on L239
+const int ml_en = 24; //Motor 2 PWM Speed Control Pin, Pin 9 on L239
 
-const int m1_da = 28; //Motor 1 Directional Control A, Pin 2 on L239
-const int m1_db = 29; //Motor 1 Directional Control B, Pin 7 on L239
-const int m2_da = 41; //Motor 2 Directional Control A, Pin 15 on L239
-const int m2_db = 40; //Motor 2 Directional Control B, Pin 10 on L239
+const int mr_da = 28; //Motor 1 Directional Control A, Pin 2 on L239
+const int mr_db = 29; //Motor 1 Directional Control B, Pin 7 on L239
+const int ml_da = 41; //Motor 2 Directional Control A, Pin 15 on L239
+const int ml_db = 40; //Motor 2 Directional Control B, Pin 10 on L239
 
-Motors motors(m1_en, m2_en, m1_da, m1_db, m2_da, m2_db);
+Motors motors(mr_en, ml_en, mr_da, mr_db, ml_da, ml_db);
 void setup() {
 
 }
