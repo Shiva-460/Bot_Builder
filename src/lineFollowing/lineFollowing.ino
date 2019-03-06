d01fc685f4cbd49b1023f8069d66a4b97c1cdd18
cmarisco <45523456+cmarisco@users.noreply.github.com>
2019-03-06 15:14:36 -0800

    Add files via upload

--- src/lineFollowing/lineFollowing.ino
+++ src/lineFollowing/lineFollowing.ino
@@ -1,15 +1,17 @@
 #include "LineSensor.h"
 #include "Motors.h"
+#include "Wires.h"
+#include "lineFollowing.h"
 
-LineSensor leftSide(45), rightSide(44);
+LineSensor leftSide(LEFT_LINE_SENSOR), rightSide(RIGHT_LINE_SENSOR);
 
-const int mr_en = 25; //Motor 1 PWM Speed Control Pin, Pin 1 on L239
-const int ml_en = 24; //Motor 2 PWM Speed Control Pin, Pin 9 on L239
+const int mr_en = RIGHT_MOTOR_ENABLE; //Motor 1 PWM Speed Control Pin, Pin 1 on L239
+const int ml_en = LEFT_MOTOR_ENABLE; //Motor 2 PWM Speed Control Pin, Pin 9 on L239
 
-const int mr_da = 28; //Motor 1 Directional Control A, Pin 2 on L239
-const int mr_db = 29; //Motor 1 Directional Control B, Pin 7 on L239
-const int ml_da = 41; //Motor 2 Directional Control A, Pin 15 on L239
-const int ml_db = 40; //Motor 2 Directional Control B, Pin 10 on L239
+const int mr_da = RIGHT_MOTOR_TOP_WIRE; //Motor 1 Directional Control A, Pin 2 on L239
+const int mr_db = RIGHT_MOTOR_BOTTOM_WIRE; //Motor 1 Directional Control B, Pin 7 on L239
+const int ml_da = LEFT_MOTOR_TOP_WIRE; //Motor 2 Directional Control A, Pin 15 on L239
+const int ml_db = LEFT_MOTOR_BOTTOM_WIRE; //Motor 2 Directional Control B, Pin 10 on L239
 
 Motors motors(mr_en, ml_en, mr_da, mr_db, ml_da, ml_db);
 
@@ -19,6 +21,6 @@ void setup() {
 
 void loop() {
   
-  lineFollow(motors, leftSide,rightSide);
-
+ lineFollow(motors, leftSide,rightSide);
+ // motors.right();
 }
